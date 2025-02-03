a4fa7e6e8900d2d0ffdb8ef127b5a3c19ce7749f
Raven The wolf <theoccultcorner@gmail.com>
2025-02-03 08:33:50 -0800

    new

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -33,7 +33,7 @@ const AboutPage = () => {
       </Typography>
 
       {/* Description Text */}
-      <Typography variant="body1" sx={{ maxWidth: '600px' }}>
+      <Typography variant="body1" sx={{ maxWidth: '600px',  marginBottom: '20px' }}>
       At Sharing The Message, we are a Narcotics Anonymous group dedicated to carrying the message of hope and recovery to the addict who still suffers. We believe in the spiritual principle that "an addict, any addict, can stop using drugs, lose the desire to use, and find a new way to live." Our group is a safe and welcoming space where no addict need ever feel alone again. Through the NA program, the Twelve Steps, and the power of one addict helping another, we walk this journey of recovery together, free from active addiction. Whether you're new to recovery, returning, or simply seeking support, you are welcome here. As the Basic Text reminds us, "We are not alone. The therapeutic value of one addict helping another is without parallel." You never have to use again—there is hope, and we are here to share it with you.
       </Typography>
     </Box>
