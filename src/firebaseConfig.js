61fdbdc85866d05f0f3f865ab736c74ac5418583
Nevarez Marketing <35445349+theoccultcorner@users.noreply.github.com>
2025-02-09 01:39:39 -0800

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";  
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0dPrA5J7xz94P-Ij3jUSIQNKCtIII500",
-  authDomain: "sharingthemessage-47e08.firebaseapp.com",
-  databaseURL: "https://sharingthemessage-47e08-default-rtdb.firebaseio.com",
-  projectId: "sharingthemessage-47e08",
-  storageBucket: "sharingthemessage-47e08.appspot.com",  
-  messagingSenderId: "600369161729",
-  appId: "1:600369161729:web:6f6d2a3a929517a962c200",
-  measurementId: "G-6TQPNXT2EK",
+apiKey: process.env.REACT_APP_FIREBASE,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,  
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
